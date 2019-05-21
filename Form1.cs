d821318987ddc85f81caf362d999bed572e04298
Julia Petrash <36237100+juliapertash@users.noreply.github.com>
2019-05-21 21:15:42 +0300

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ev1_Click(object sender, EventArgs e)
+        {
+            string message = Mess.Text;
+            Regex reg = new Regex("[0-1][0-1][0-1][0-1]");
+            if(Regex.IsMatch(message, reg.ToString()))
+            {
+                int[] mess = new int[4];
+                for (int i = 0; i < 4; i++)
+                {
+                    mess[i] = Convert.ToInt16(message[i]);
+                }
+                int sum1 = mess[1] + mess[2] + mess[3];
+                int p4;
+                if (sum1 % 2 == 0)
+                    p4 = 0;
+                else p4 = 1;
+                int sum2 = mess[0] + mess[2] + mess[3];
+                int p2;
+                if (sum2 % 2 == 0)
+                    p2 = 0;
+                else p2 = 1;
+                int sum3 = mess[0] + mess[3] + mess[1];
+                int p1;
+                if (sum3 % 2 == 0)
+                    p1 = 0;
+                else p1 = 1;
+                string output = $"{p1}{p2}{message[0]}{p4}{message[1]}{message[2]}{message[3]}";
+                Mess_code.Text = output;
+            }
+        }
+
+        private void find_Click(object sender, EventArgs e)
+        {
+            string code = wrongmess.Text;
+            Regex reg1 = new Regex("[0-1][0-1][0-1][0-1][0-1][0-1][0-1]");
+            if (Regex.IsMatch(code, reg1.ToString()))
+            {
+                int[] codenums = new int[7];
+                for (int i = 0; i < 7; i++)
+                {
+                    codenums[i] = (int)code[i];
+                }
+                int sum1 = codenums[0] + codenums[2] + codenums[4] + codenums[6];
+                int n3;
+                if (sum1 % 2 == 0)
+                    n3 = 0;
+                else n3 = 1;
+                int sum2 = codenums[1] + codenums[2] + codenums[5] + codenums[6];
+                int n2;
+                if (sum2 % 2 == 0)
+                    n2 = 0;
+                else n2 = 1;
+                int sum3 = codenums[3] + codenums[4] + codenums[5] + codenums[6];
+                int n1;
+                if (sum3 % 2 == 0)
+                    n1 = 0;
+                else n1 = 1;
+                int k = 0;
+                string dvoistr = $"{n1}{n2}{n3}";
+                char[] ar = dvoistr.ToCharArray();
+                Array.Reverse(ar); //Разворачивем массив
+                dvoistr = new String(ar); //Записываем развернутый массив в строку
+
+                for (int i = 0; i < dvoistr.Length; i++) //Посимвольно читаем строку
+                {
+                    k += Convert.ToInt32(dvoistr[i].ToString()) * Convert.ToInt32(Math.Pow(2, i)); //умножаем каждый имвол на 2 в соответствующей степени (степень = позиция)
+                }
+                if (k != 0)
+                {
+                    if (codenums[k - 1] == 49)
+                        codenums[k - 1] = 48;
+                    else codenums[k - 1] = 49;
+                }
+                string output = "";
+                foreach (var a in codenums)
+                {
+                    output += (char)a;
+                }
+                final.Text = output;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "▀ тїюфшы тьхёЄю фшъюую чтхЁ т ъыхЄъє,";
+            byte[] bytes = Encoding.GetEncoding(866).GetBytes(str);
+            string newStr = Encoding.GetEncoding(1251).GetString(bytes);
+            Console.WriteLine(newStr);
+            char[] alphabet = {'_','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l','m','n','o','p','q','r','s','t','u', 'v','w','x','y','z'};
+            Console.WriteLine("***ШИФРОВКА ЦЕЗАРЯ***");
+            Console.WriteLine("Введите сообщение");
+            string message = Console.ReadLine();
+            int key=-1;
+            while (key < 0 || key > 26)
+            {
+                Console.WriteLine("Введите ключ");
+                key = Convert.ToInt16(Console.ReadLine());
+            }
+            Console.WriteLine("Результат");
+            string output= "";
+            foreach(char a in message)
+            {
+                int index=0;
+                for (int i = 0; i < alphabet.Length; i++)
+                {
+                if(alphabet[i].Equals(a))
+                {
+                        index = i + key;
+                        if (index > 26)
+                            index = index - 26;
+                }
+                }
+                output += alphabet[index];
+            }
+            Console.WriteLine(output);
+            Console.WriteLine("***ЦЕЗАРЬ. ДЕКОДИРОВКА***");
+            Console.WriteLine("Введите зашифрованное сообщение");
+            string str1 = Console.ReadLine();
+            for (int i = 1; i < 27; i++)
+            {
+                string outputt = "";
+                foreach (char a in str1)
+                {
+
+                    int index = 0;
+                    for (int s = 0; s < alphabet.Length; s++)
+                    {
+                        if (alphabet[s].Equals(a))
+                        {
+                            index = s + i;
+                            if (index > 26)
+                                index = index - 26;
+                        }
+                    }
+                    outputt += alphabet[index];
+                }
+                Console.WriteLine($"Ключ: {i}.     Строка: {outputt}");
+            }
+
+
+        }
+    }
+}
